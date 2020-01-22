cd4c9219a773b7f85e0e2bca5482c3537abbe4fa
Jeremy Brady <jbdigitalgeek@gmail.com>
2020-01-22 13:01:38 -0800

    selection sort logic working I think

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,11 +3,15 @@ def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
+        cur_val = arr[i]
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[i] = arr[smallest_index]
+        arr[smallest_index] = cur_val
 
 
         # TO-DO: swap
